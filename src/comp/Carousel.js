f7fc732a44aadbefb5e03d3dc1dcf3e510849eee
tomdamo <20182376@student.hhs.nl>
2023-08-19 23:43:16 +0200

    potentially fix bug reloading page
    removed the animations, I think something went wrong with the animation if you refresh the same page. Because the images don't load up after refreshing - weirdly enough this doesn't happen at the production area, running it local.

--- src/comp/Carousel.js
+++ src/comp/Carousel.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { homeImages } from "./images";
+import React, { useState } from "react";
 import {
   Carousel,
   CarouselItem,
@@ -7,6 +6,7 @@ import {
   CarouselIndicators,
 } from "reactstrap";
 
+import { homeImages } from "./images";
 import "./Carousel.css";
 
 const items = homeImages.map((image, index) => ({
@@ -15,89 +15,66 @@ const items = homeImages.map((image, index) => ({
   caption: `Slide ${index + 1}`,
   key: index,
 }));
+
 export function CarouselComp(args) {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-  const [loadedImages, setLoadedImages] = useState([]);
-
-  useEffect(() => {
-    setLoadedImages(homeImages.map(() => false));
-  }, []);
-
-  const handleImageLoad = (index) => {
-    setLoadedImages((prevLoadedImages) => {
-      const newLoadedImages = [...prevLoadedImages];
-      newLoadedImages[index] = true;
-      return newLoadedImages;
-    });
-  };
 
   const next = () => {
-    if (animating) return;
     const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
 
   const previous = () => {
-    if (animating) return;
     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
 
   const goToIndex = (newIndex) => {
-    if (animating) return;
     setActiveIndex(newIndex);
   };
 
-  const slides = items.map((item, index) => {
-    return (
-      <CarouselItem
-        onExiting={() => setAnimating(true)}
-        onExited={() => setAnimating(false)}
-        key={item.src}
-      >
-        <img
-          src={item.src}
-          alt={item.altText}
-          className={`carousel-image ${loadedImages[index] ? "loaded" : ""}`}
-          onLoad={() => handleImageLoad(index)}
-        />
-        {/* <CarouselCaption
-          captionText={item.caption}
-          captionHeader={item.caption}
-        /> */}
-      </CarouselItem>
-    );
-  });
+  const slides = items.map((item) => (
+    <CarouselItem
+      key={item.src}
+    >
+      <img
+        src={item.src}
+        alt={item.altText}
+        className="carousel-image"
+      />
+      {/* <CarouselCaption
+        captionText={item.caption}
+        captionHeader={item.caption}
+      /> */}
+    </CarouselItem>
+  ));
 
   return (
-    <>
-      <div className="carousel-container">
-        <Carousel
+    <div className="carousel-container">
+      <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+        {...args}
+      >
+        <CarouselIndicators
+          items={items}
           activeIndex={activeIndex}
-          next={next}
-          previous={previous}
-          {...args}
-        >
-          <CarouselIndicators
-            items={items}
-            activeIndex={activeIndex}
-            onClickHandler={goToIndex}
-          />
-          {slides}
-          <CarouselControl
-            direction="prev"
-            directionText="Previous"
-            onClickHandler={previous}
-          />
-          <CarouselControl
-            direction="next"
-            directionText="Next"
-            onClickHandler={next}
-          />
-        </Carousel>
-      </div>
-    </>
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={next}
+        />
+      </Carousel>
+    </div>
   );
 }
 
--- src/comp/ImageGallery.js
+++ src/comp/ImageGallery.js
@@ -1,41 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./ImagesGallery.css";
 
 function ImageGallery({ images }) {
   const [isFullScreen, setIsFullScreen] = useState(false);
   const [currentImage, setCurrentImage] = useState("");
-  const [loadedImages, setLoadedImages] = useState([]);
 
   const handleClick = (image) => {
     setIsFullScreen(true);
     setCurrentImage(image);
   };
 
-  useEffect(() => {
-    setLoadedImages(images.map(() => false));
-  }, [images]);
-
-  const handleImageLoad = (index) => {
-    setLoadedImages((prevLoadedImages) => {
-      const newLoadedImages = [...prevLoadedImages];
-      newLoadedImages[index] = true;
-      return newLoadedImages;
-    });
-  };
-
   return (
     <div className="photo-grid-container">
       <div className="photo-grid">
         {images.map((image, index) => (
-          <div
-            className={`card ${loadedImages[index] ? "loaded" : ""}`}
-            key={index}
-          >
+          <div className="card" key={index}>
             <img
               src={image.src}
               alt={image.alt}
               onClick={() => handleClick(image.src)}
-              onLoad={() => handleImageLoad(index)}
             />
             <figcaption>{image.caption}</figcaption>
           </div>
