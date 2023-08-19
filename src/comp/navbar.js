f871b5ea5c6b12d938f126f89252bd051d781f50
tomdamo <20182376@student.hhs.nl>
2023-08-19 02:18:16 +0200

    navbar change

--- src/comp/navbar.js
+++ src/comp/navbar.js
@@ -1,26 +1,29 @@
+import React from "react";
+import { useLocation } from "react-router-dom"; // Assuming you are using React Router
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "./nav.css";
+
 function Navi() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <Navbar bg="light" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Brand href="/">{isHomePage ? "Home" : "Caja Boogers"}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-          <Nav.Link href="/Work">Work</Nav.Link>
+            <Nav.Link href="/Work">Work</Nav.Link>
             <Nav.Link href="/Contact">Contact</Nav.Link>
             <Nav.Link href="/Cv">CV</Nav.Link>
           </Nav>
           <Nav className="instagram-container">
-            <Nav.Link
-              href="https://www.instagram.com/cajaboogers/"
-              target="_blank"
-            >
+            <Nav.Link href="https://www.instagram.com/cajaboogers/" target="_blank">
               <FontAwesomeIcon icon={faInstagram} className="instagram-icon" />
             </Nav.Link>
           </Nav>
