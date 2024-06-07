520e03858f8c2a93846dc050b6ffa5d5ac47b5b0
tomdamo <20182376@student.hhs.nl>
2024-06-08 01:35:23 +0200

    load logo first

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -1,30 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { CarouselComp } from "./Carousel";
-
 import "./Home.css";
 import { Container } from "react-bootstrap";
 import CarouselCompLang from "./CarouselLang";
 
 function Home(args) {
   const homeImageUrl = "https://imagedelivery.net/wnsKQets6eRCGizoqWHVjg/39f78907-4fc0-4aad-e4ec-5889b57b8d00/public";
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <>
-      <div className="Title"><img src={homeImageUrl} width="130px" height="100%" alt="caja"/></div>
-      <div> </div>
-      <div className="grid-container">
-        <div className="carousel">
-          <CarouselCompLang />
-        </div>
-        <div className="hero">
-          <div className="hero-content">
-            <Container>
-            </Container>
-            <Container>
-              <CarouselComp />
-            </Container>
+      <div className="Title">
+        <img 
+          src={homeImageUrl} 
+          width="130px" 
+          height="100%" 
+          alt="caja" 
+          onLoad={() => setImageLoaded(true)} // Set imageLoaded to true when the image has loaded
+        />
+      </div>
+      {imageLoaded && (
+        <div>
+          <div className="grid-container">
+            <div className="carousel">
+              <CarouselCompLang />
+            </div>
+            <div className="hero">
+              <div className="hero-content">
+                <Container>
+                </Container>
+                <Container>
+                  <CarouselComp />
+                </Container>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
